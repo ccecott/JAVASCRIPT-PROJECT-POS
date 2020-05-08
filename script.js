a26ca2fea1ac9e258cc77a93b4bcba75bbd7a081
jakembuck <63891892+jakembuck@users.noreply.github.com>
2020-05-08 15:06:27 -0400

    ugh

--- script.js
+++ script.js
@@ -1,23 +1,64 @@
-let popup = document.querySelector(".pop");
+// Popup Menus and whatever
+// Backgrounds popup menu
+let bpop = document.querySelector(".bpop");
 let backgroundPop = document.querySelector(".background-popup");
-const addFunc = () => {
-    popup.style.display = "flex";
+const addFuncBpop = () => {
+  bpop.style.display = "flex";
 }
-backgroundPop.addEventListener("click", addFunc);
+const removeFuncBpop = () => {
+  bpop.style.display = "none";
+}
+backgroundPop.addEventListener("click", addFuncBpop);
 
-let extensionsPop = document.querySelector(".extensions-popup");
-extensionsPop.addEventListener("click", addFunc);
+// Extensions popup menu
+// let epop = document.querySelector(".epop");
+// let extensionsPop = document.querySelector(".extensions-popup");
+// const addFuncEpop = () => {
+//   epop.style.display = "flex";
+// }
+// const removeFuncEpop = () => {
+//   epop.style.display = "none";
+// }
+// extensionsPop.addEventListener("click", addFuncEpop);
 
+// Products popup menu
+// let ppop = document.querySelector(".ppop");
+// let productsPop = document.querySelector(".products-popup");
+// const addFuncPpop = () => {
+//   ppop.style.display = "flex";
+// }
+// const removeFuncPpop = () => {
+//   ppop.style.display = "none";
+// }
+// productsPop.addEventListener("click", addFuncPpop);
 
-let productsPop = document.querySelector(".products-popup");
-productsPop.addEventListener("click", addFunc);
+// Checkout Popup
+let cpop = document.querySelector(".fa-shopping-cart");
+let checkoutPop = document.querySelector(".checkout-style");
+const addFuncCpop = () => {
+  checkoutPop.style.display = "flex";
+}
+// const removeFuncCpop = () => {
+//   cpop.style.display = "none";
+// }
+cpop.addEventListener("click", addFuncCpop);
 
-let body = document.querySelector(".full-body");
+// Exit button for Popup menus
+let exitButton = document.querySelector(".fa-times");
+exitButton.addEventListener("click", removeFuncBpop);
 
-body.addEventListener("click", (e) => {
-    if (e.target.classList.contains("body")) {
-        // e.target.removeEventListener();
-        popup.style.display = "none"
-    }
-});
+// Cart and Checkout functions and such
+let addToCart = document.querySelectorAll(".add-to-cart");
+let cartDeets = document.querySelector(".checkout-deets");
+let cart = document.querySelector(".fa-shopping-cart");
 
+addToCart.addEventListener("click", (event) => {
+  addToCart.forEach(() => {
+    let title = event.target.getAttribute("data-title");
+    let info = event.target.getAttribute("data-info");
+    let price = event.target.getAttribute("data-price");
+    let cartItem = document.createElement("div");
+    cartItem.append(title, price);
+    cartDeets.append(cartItem);
+  })
+})
