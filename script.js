0b63ea2c89e02a7c05364f789a3e4901b6b75a23
Julie Smyth <juliemcsmyth@gmail.com>
2020-05-08 17:59:58 -0400

    tax calculation

--- script.js
+++ script.js
@@ -39,9 +39,9 @@ let checkoutPop = document.querySelector(".cart-container");
 const addFuncCpop = () => {
   checkoutPop.style.display = "flex";
 }
-// const removeFuncCpop = () => {
-//   cpop.style.display = "none";
-// }
+const removeFuncCpop = () => {
+  checkoutPop.style.display = "none";
+}
 cpop.addEventListener("click", addFuncCpop);
 
 // Exit button for Popup menus
@@ -54,37 +54,52 @@ eexitButton.addEventListener("click", removeFuncEpop);
 let pexitButton = document.querySelector(".pexit");
 pexitButton.addEventListener("click", removeFuncPpop);
 
+let cexitButton = document.querySelector(".cexit");
+cexitButton.addEventListener("click", removeFuncCpop);
+
 // Cart and Checkout functions and such
 
 let cartDeets = document.querySelector(".checkout-deets");
 let cart = document.querySelector(".fa-shopping-cart");
 
 fullBody.addEventListener("click", (event) => {
-    if (event.target.classList.contains("add-to-cart")) {
-        let cartItem = document.createElement("div");
-        let titleP = document.createElement("p");
-        let title = event.target.getAttribute("data-title");
-        titleP.innerText = title;
-        let priceP = document.createElement("p");
-        let price = event.target.getAttribute("data-price");
-        priceP.classList.add("price");
-        priceP.innerText = price;
-        cartItem.append(titleP, priceP);
-        cartDeets.append(cartItem);
-    }
+  if (event.target.classList.contains("add-to-cart")) {
+    let cartItem = document.createElement("div");
+    let titleP = document.createElement("p");
+    let title = event.target.getAttribute("data-title");
+    titleP.innerText = title;
+    let priceP = document.createElement("p");
+    let price = event.target.getAttribute("data-price");
+    priceP.classList.add("price");
+    priceP.innerText = price;
+    cartItem.append(titleP, priceP);
+    cartDeets.append(cartItem);
+  }
 });
 
 
 
 let currentSubTotal = 0;
 let subTotal = document.querySelector(".subtotal");
+let currentTax = 0;
+let tax = document.querySelector(".tax");
+let currentTotal = 0;
+let total = document.querySelector(".total");
 let cartContainer = document.querySelector(".cart-container");
 
 fullBody.addEventListener("click", (event) => {
-    if (event.target.classList.contains("add-to-cart")) {
-        let amount = Number(event.target.getAttribute("data-price"));
-        currentSubTotal += amount;
-        subTotal.innerText = `Subtotal $${currentSubTotal.toFixed(2)}`;
-    }
+  if (event.target.classList.contains("add-to-cart")) {
+    let amount = Number(event.target.getAttribute("data-price"));
+    currentSubTotal += amount;
+    subTotal.innerText = `Subtotal $${currentSubTotal.toFixed(2)}`;
+  }
+});
+
+fullBody.addEventListener("click", (event) => {
+  if (event.target.classList.contains("add-to-cart")) {
+    let taxAmount = Number(event.target.getAttribute("data-price"));
+    currentTax = taxAmount *= .06;
+    tax.innerText = `Tax $${currentTax.toFixed(2)}`;
+  }
 });
 
