53973a600ed4915bc44f9e1a979f697d9fe3d41b
Cassie Cecott <casscecott@gmail.com>
2020-05-08 09:59:05 -0500

    pop ups enabled

---
+++ script.js
@@ -0,0 +1,15 @@
+let popup = document.querySelector(".pop");
+let backgroundPop = document.querySelector(".background-popup");
+const addFunc = () => {
+    popup.style.display = "flex";
+}
+backgroundPop.addEventListener("click", addFunc);
+
+let extensionsPop = document.querySelector(".extensions-popup");
+extensionsPop.addEventListener("click", addFunc);
+
+
+let productsPop = document.querySelector(".products-popup");
+productsPop.addEventListener("click", addFunc);
+
+
